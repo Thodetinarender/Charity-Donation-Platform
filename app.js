6927ab4fe579ea97ba8d832cd4d80f3158d24dbd
THODETI NARENDER <thodetinarender51@gmail.com>
2025-07-03 05:15:15 +0530

    updated port

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ process.on('unhandledRejection', (reason, promise) => {
     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-sequelize.sync({ force: true }).then(() => { // Avoid using alter: true true
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+sequelize.sync({ force: false }).then(() => { // Avoid using alter: true true
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
   });
 });
