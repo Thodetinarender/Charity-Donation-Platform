c8b85e596c99f326be51308403909b38c493e142
THODETI NARENDER <thodetinarender51@gmail.com>
2025-07-03 11:45:00 +0530

    remove admin

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ const User = sequelize.define('User', {
   },
   isAdmin: {
     type: DataTypes.BOOLEAN,
-    defaultValue: true,
+    defaultValue: false,
   },
   street: {
     type: DataTypes.STRING,
